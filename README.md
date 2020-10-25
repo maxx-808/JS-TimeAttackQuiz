9cb15df23dd3e1fe5e4f967ea82eda992cb99d2c
Maxx <maxhiga.hawaii@gmail.com>
2020-10-24 16:27:14 -1000

    last bit of work that I can do

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ homework 4 Web APIs, Javascript quiz timed
 
 this is my homework for week 4 Web API's
 It is a mulitple choice 7 question timed quiz with a highscore leaderboard
+60 seconds to take the quiz and 5 seconds deducted for every wrong answer
+the score is your time left and you can type in your initials to save to local storage together
+this quiz is about Javscript and should be fairly easy for anyone who did a small bit of learning about Javascript
+
+I could not get the code to work that brings back the highscores from local storage nor could I set it up to take the top 10 highest scores.
+
--- app.js
+++ app.js
@@ -8,6 +8,13 @@ var timerEl = document.querySelector("#timer");
 var startButton = document.querySelector("#start-button");
 var startPage = document.querySelector("#start-page");
 var quizPage = document.querySelector("#question-page");
+var scorePage = document.querySelector("#highscore-page");
+var score1span = document.querySelector("#score1");
+var score2span = document.querySelector("#score2");
+var score3span = document.querySelector("#score3");
+var viewHighscore = document.querySelector("#veiw-highscores")
+var timeIdentifier = document.querySelector("#time-identifier");
+var homeButton = document.querySelector("#home");
 
 // questions variables
 var questionsPointer = 0;
@@ -19,45 +26,84 @@ var optionC = document.querySelector("#optionC");
 //setting html timer to have the count in writing
 timerEl.textContent = timerCount
 
+//highscore function
+viewHighscore.addEventListener("click", function () {
+    startPage.setAttribute("class", "container hide");
+    quizPage.setAttribute("class", "container hide");
+    timerEl.setAttribute("class", "hide");
+    scorePage.setAttribute("class", "");
+    viewHighscore.setAttribute("class", "hide");
+    timeIdentifier.setAttribute("class", "hide");
+})
+
+homeButton.addEventListener("click", function() {
+    window.location.reload();
+})
+
 //timer variable set to clear at 0 seconds
 startButton.addEventListener("click", function() {
-
+    // timer = setInterval
     var timer = window.setInterval(function() {
-        timerCount--;
-        timerEl.textContent = timerCount;
+
+        if (setQuestions) {
+            timerCount--;
+            timerEl.textContent = timerCount;
+        }    
         
         if (timerCount <= 0 || questionsPointer === questions.length) {
             clearInterval(timer);
             quizPage.setAttribute("class", "container hide");
             startPage.setAttribute("class", "container");
+            timerEl.setAttribute("class", "hide");
             alert("You have run out of time. Reload page and try again.");
-            return;
+            if (alert) {
+                timerCount = 60;
+                window.location.reload();
+            }
         }
-
+    
     }, 1000);
     
     setQuestions();
 
     startPage.setAttribute("class", "container hide");
     quizPage.setAttribute("class", "container");
+    timerEl.setAttribute("class", "");
     timerCount = 60;
     timerEl.removeAttribute("class");
 
-
 });
-
 
 // function to run through the questions
 function setQuestions() {
+    clearInterval(timer);
 
     if (questionsPointer === questions.length) {
+        // clearInterval(timer);
         alert("you are done with " + timerCount + " out of 60 seconds");
         console.log(timerCount);
+        var scoreInitial = prompt("Please write initials to add to your score.")
+
+        //highscore variables
+        var scoreStore = {
+            initial: scoreInitial,
+            timeLeft: timerCount,
+        }
+
+        localStorage.setItem("Score save", JSON.stringify(scoreStore));
+
+        // var lastHighscore = localStorage.getItem("Score save");
+        // score1span.textContent = lastHighscore.timerLeft;
+        //was not able to get this to work
+
         quizPage.setAttribute("class", "container hide")
         startPage.setAttribute("class", "contianer")
         timerEl.setAttribute("class", "hide");
         questionsPointer = 0;
         timerCount = 60;
+        if (prompt) {
+            window.location.reload();
+        };
     };
 
     question.textContent = questions[questionsPointer].q;
